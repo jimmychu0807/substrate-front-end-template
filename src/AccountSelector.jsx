bb7cee9851311805fc48cbff5ea447bc5bd6298b
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-09-09 22:31:04 +0200

    Refactor Account Selector

---
+++ src/AccountSelector.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+import { Menu, Dropdown, Container, Icon } from "semantic-ui-react";
+
+export default function NodeInfo(props) {
+  const { keyring, setAccountAddress } = props;
+  const [accountSelected, setAccountSelected] = useState("");
+
+  // get the list of accounts we possess the private key for
+  const keyringOptions = keyring.getPairs().map(account => ({
+    key: account.address,
+    value: account.address,
+    text: account.meta.name.toUpperCase(),
+    icon: "user"
+  }));
+
+  return (
+    <Menu
+      attached="top"
+      tabular
+      style={{
+        backgroundColor: "#fff",
+        borderColor: "#fff",
+        paddingTop: "1em",
+        paddingBottom: "1em"
+      }}
+    >
+      <Container>
+        <Menu.Menu position="right">
+          <Icon
+            name="users"
+            size="large"
+            circular
+            color={accountSelected ? "green" : "red"}
+          ></Icon>
+          <Dropdown
+            search
+            selection
+            clearable
+            placeholder="Select an account"
+            options={keyringOptions}
+            onChange={(_, dropdown) => {
+              setAccountAddress(dropdown.value);
+              setAccountSelected(dropdown.value);
+            }}
+          />
+        </Menu.Menu>
+      </Container>
+    </Menu>
+  );
+}
