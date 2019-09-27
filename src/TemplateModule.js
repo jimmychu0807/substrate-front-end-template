2fcda5990ebff5c78746696473bef0425ad823f8
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-09-27 12:05:07 +0100

    fix deps

--- src/TemplateModule.js
+++ src/TemplateModule.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Form, Input, Grid, Card, Statistic } from "semantic-ui-react";
 
-import { useSubstrate } from "../substrate-lib";
-import { TxButton } from "../substrate-lib/components";
+import { useSubstrate } from "./substrate-lib";
+import { TxButton } from "./substrate-lib/components";
 
 export default function Transfer(props) {
   const { api } = useSubstrate();
