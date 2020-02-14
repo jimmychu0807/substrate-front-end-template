f2f59c06ac0258664a6fc923841790c511e20d67
Jimmy Chu <jimmychu0807@gmail.com>
2020-02-14 14:40:24 +0800

    Detect disconnection and reconnect when substrate node resumed

--- src/substrate-lib/useSubstrate.js
+++ src/substrate-lib/useSubstrate.js
@@ -16,16 +16,14 @@ const useSubstrate = () => {
     if (api) return;
 
     const provider = new WsProvider(socket);
+    const _api = new ApiPromise({ provider, types });
 
-    try {
-      const _api = await ApiPromise.create({ provider, types });
+    _api.on('connected', () => {
       dispatch({ type: 'CONNECT', payload: _api });
-      await _api.isReady;
-      dispatch({ type: 'CONNECT_SUCCESS' });
-    } catch (e) {
-      console.error(e);
-      dispatch({ type: 'CONNECT_ERROR' });
-    }
+      _api.isReady.then((_api) => dispatch({ type: 'CONNECT_SUCCESS' }));
+    });
+    _api.on('ready', () => dispatch({ type: 'CONNECT_SUCCESS' }));
+    _api.on('error', () => dispatch({ type: 'CONNECT_ERROR' }));
   }, [api, socket, types, dispatch]);
 
   // hook to get injected accounts
