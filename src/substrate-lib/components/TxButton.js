a265933bef4b632372afb924bc2544b30f7f18a7
Jimmy Chu <jimmychu0807@gmail.com>
2020-06-05 22:06:25 +0800

    Fixing type be not existing

--- src/substrate-lib/components/TxButton.js
+++ src/substrate-lib/components/TxButton.js
@@ -134,10 +134,10 @@ function TxButton ({
   const transformParams = (paramFields, inputParams, opts = { emptyAsNull: true }) => {
     // if `opts.emptyAsNull` is true, empty param value will be added to res as `null`.
     //   Otherwise, it will not be added
-    const paramVal = inputParams.map(inputParam => typeof inputParam === 'object' ? inputParam.value.trim() : inputParam);
+    const paramVal = inputParams.map(inputParam => typeof inputParam === 'object' ? inputParam.value.trim() : inputParam.trim());
     const params = paramFields.map((field, ind) => ({ ...field, value: paramVal[ind] || null }));
 
-    return params.reduce((memo, { type, value }) => {
+    return params.reduce((memo, { type = 'string', value }) => {
       if (value == null || value === '') return (opts.emptyAsNull ? [...memo, null] : memo);
 
       let converted = value;
