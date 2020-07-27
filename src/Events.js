16be22b74e24aeb9b76cc6b93b623d91dab85be3
Jimmy Chu <jimmy@parity.io>
2020-07-27 20:01:36 +0800

    Event unsubscription and allow event feed maxHeight to be set from props (#118)

--- src/Events.js
+++ src/Events.js
@@ -3,48 +3,53 @@ import { Feed, Grid, Button } from 'semantic-ui-react';
 
 import { useSubstrate } from './substrate-lib';
 
+// Events to be filtered from feed
+const FILTERED_EVENTS = [
+  'system:ExtrinsicSuccess:: (phase={"ApplyExtrinsic":0})',
+  'system:ExtrinsicSuccess:: (phase={"ApplyExtrinsic":1})'
+];
+
 function Main (props) {
   const { api } = useSubstrate();
   const [eventFeed, setEventFeed] = useState([]);
 
   useEffect(() => {
-    // Filter some event from feed
-    const filter = [
-      'system:ExtrinsicSuccess:: (phase={"ApplyExtrinsic":0})',
-      'system:ExtrinsicSuccess:: (phase={"ApplyExtrinsic":1})'
-    ];
+    let unsub = null;
+    const allEvents = async () => {
+      unsub = await api.query.system.events(events => {
+        // loop through the Vec<EventRecord>
+        events.forEach(record => {
+          // extract the phase, event and the event types
+          const { event, phase } = record;
+          const types = event.typeDef;
 
-    api.query.system.events(events => {
-      // loop through the Vec<EventRecord>
-      events.forEach(record => {
-        // extract the phase, event and the event types
-        const { event, phase } = record;
-        const types = event.typeDef;
+          // show what we are busy with
+          const eventName = `${event.section}:${
+            event.method
+          }:: (phase=${phase.toString()})`;
 
-        // show what we are busy with
-        const eventName = `${event.section}:${
-          event.method
-        }:: (phase=${phase.toString()})`;
+          if (FILTERED_EVENTS.includes(eventName)) return;
 
-        if (filter.includes(eventName)) return;
+          // loop through each of the parameters, displaying the type and data
+          const params = event.data.map(
+            (data, index) => `${types[index].type}: ${data.toString()}`
+          );
 
-        // loop through each of the parameters, displaying the type and data
-        const params = event.data.map(
-          (data, index) => `${types[index].type}: ${data.toString()}`
-        );
-
-        setEventFeed(e => [
-          {
+          setEventFeed(e => [{
             icon: 'bell',
             summary: `${eventName}-${e.length}`,
             extraText: event.meta.documentation.join(', ').toString(),
             content: params.join(', ')
-          },
-          ...e
-        ]);
+          }, ...e]);
+        });
       });
-    });
+    };
+
+    allEvents();
+    return () => unsub && unsub();
   }, [api.query.system]);
+
+  const { feedMaxHeight = 250 } = props;
 
   return (
     <Grid.Column width={8}>
@@ -57,7 +62,7 @@ function Main (props) {
         icon='erase'
         onClick={ _ => setEventFeed([]) }
       />
-      <Feed style={{ clear: 'both', overflow: 'auto', maxHeight: 250 }} events={eventFeed} />
+      <Feed style={{ clear: 'both', overflow: 'auto', maxHeight: feedMaxHeight }} events={eventFeed} />
     </Grid.Column>
   );
 }
