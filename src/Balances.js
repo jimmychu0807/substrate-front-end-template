5bd2a5848e2ecea6994a60992e69bc7cadcf136c
Dan Forbes <dan@danforbes.dev>
2020-12-23 15:28:50 -0800

    Nitpicks

--- src/Balances.js
+++ src/Balances.js
@@ -42,29 +42,29 @@ export default function Main (props) {
             </Table.Cell>
           </Table.Row>
           {accounts.map(account =>
-          <Table.Row key={account.address}>
-            <Table.Cell width={3} textAlign='right'>{account.meta.name}</Table.Cell>
-            <Table.Cell width={10}>
-              <span style={{ display: 'inline-block', minWidth: '31em' }}>
-                {account.address}
-              </span>
-              <CopyToClipboard text={account.address}>
-                <Button
-                  basic
-                  circular
-                  compact
-                  size='mini'
-                  color='blue'
-                  icon='copy outline'
-                />
-              </CopyToClipboard>
-            </Table.Cell>
-            <Table.Cell width={3}>{
-              balances && balances[account.address] &&
-              balances[account.address]
-            }</Table.Cell>
-          </Table.Row>
-        )}
+            <Table.Row key={account.address}>
+              <Table.Cell width={3} textAlign='right'>{account.meta.name}</Table.Cell>
+              <Table.Cell width={10}>
+                <span style={{ display: 'inline-block', minWidth: '31em' }}>
+                  {account.address}
+                </span>
+                <CopyToClipboard text={account.address}>
+                  <Button
+                    basic
+                    circular
+                    compact
+                    size='mini'
+                    color='blue'
+                    icon='copy outline'
+                  />
+                </CopyToClipboard>
+              </Table.Cell>
+              <Table.Cell width={3}>{
+                balances && balances[account.address] &&
+                balances[account.address]
+              }</Table.Cell>
+            </Table.Row>
+          )}
         </Table.Body>
       </Table>
     </Grid.Column>
