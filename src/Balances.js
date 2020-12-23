5266e971b078cba65b48be5bda6b7c719cfe14ab
HuiFeng Tang <1290017556@qq.com>
2020-12-24 07:30:03 +0800

    add Balances table titles (#155)
    * add table title
    
    * Nitpicks
    
    Co-authored-by: Dan Forbes <dan@danforbes.dev>

--- src/Balances.js
+++ src/Balances.js
@@ -29,30 +29,42 @@ export default function Main (props) {
     <Grid.Column>
       <h1>Balances</h1>
       <Table celled striped size='small'>
-        <Table.Body>{accounts.map(account =>
-          <Table.Row key={account.address}>
-            <Table.Cell width={3} textAlign='right'>{account.meta.name}</Table.Cell>
-            <Table.Cell width={10}>
-              <span style={{ display: 'inline-block', minWidth: '31em' }}>
-                {account.address}
-              </span>
-              <CopyToClipboard text={account.address}>
-                <Button
-                  basic
-                  circular
-                  compact
-                  size='mini'
-                  color='blue'
-                  icon='copy outline'
-                />
-              </CopyToClipboard>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell width={3} textAlign='right'>
+              <strong>Name</strong>
             </Table.Cell>
-            <Table.Cell width={3}>{
-              balances && balances[account.address] &&
-              balances[account.address]
-            }</Table.Cell>
+            <Table.Cell width={10}>
+              <strong>Address</strong>
+            </Table.Cell>
+            <Table.Cell width={3}>
+              <strong>Balance</strong>
+            </Table.Cell>
           </Table.Row>
-        )}
+          {accounts.map(account =>
+            <Table.Row key={account.address}>
+              <Table.Cell width={3} textAlign='right'>{account.meta.name}</Table.Cell>
+              <Table.Cell width={10}>
+                <span style={{ display: 'inline-block', minWidth: '31em' }}>
+                  {account.address}
+                </span>
+                <CopyToClipboard text={account.address}>
+                  <Button
+                    basic
+                    circular
+                    compact
+                    size='mini'
+                    color='blue'
+                    icon='copy outline'
+                  />
+                </CopyToClipboard>
+              </Table.Cell>
+              <Table.Cell width={3}>{
+                balances && balances[account.address] &&
+                balances[account.address]
+              }</Table.Cell>
+            </Table.Row>
+          )}
         </Table.Body>
       </Table>
     </Grid.Column>
