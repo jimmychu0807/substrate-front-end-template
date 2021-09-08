d80efb4345c87bf84b761018cc92505ae3388ed0
Jimmy Chu <jimmychu0807@gmail.com>
2021-09-08 12:33:41 +0800

    Shortened code on rendering kitty

--- src/KittyAvatar.js
+++ src/KittyAvatar.js
@@ -1,86 +1,20 @@
 import React from 'react';
 
+// Generate an array [start, start + 1, ..., end] inclusively
+const genArray = (start, end) =>
+  Array.from(Array(end - start + 1).keys()).map(v => v + start);
+
 const IMAGES = {
-  accessory: [
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_1.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_2.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_3.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_4.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_5.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_6.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_7.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_8.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_9.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_10.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_11.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_12.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_13.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_14.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_15.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_16.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_17.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_18.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_19.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_20.png`
-  ],
-  body: [
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_1.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_2.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_3.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_4.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_5.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_6.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_7.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_8.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_9.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_10.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_11.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_12.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_13.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_14.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_15.png`
-  ],
-  eyes: [
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_1.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_2.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_3.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_4.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_5.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_6.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_7.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_8.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_9.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_10.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_11.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_12.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_13.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_14.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_15.png`
-  ],
-  mouth: [
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/mouth_1.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/mouth_2.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/mouth_3.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/mouth_4.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/mouth_5.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/mouth_6.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/mouth_7.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/mouth_8.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/mouth_9.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/mouth_10.png`
-  ],
-  fur: [
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/fur_1.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/fur_2.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/fur_3.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/fur_4.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/fur_5.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/fur_6.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/fur_7.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/fur_8.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/fur_9.png`,
-    `${process.env.PUBLIC_URL}/assets/KittyAvatar/fur_10.png`
-  ]
+  accessory: genArray(1, 20).map(n =>
+    `${process.env.PUBLIC_URL}/assets/KittyAvatar/accessorie_${n}.png`),
+  body: genArray(1, 15).map(n =>
+    `${process.env.PUBLIC_URL}/assets/KittyAvatar/body_${n}.png`),
+  eyes: genArray(1, 15).map(n =>
+    `${process.env.PUBLIC_URL}/assets/KittyAvatar/eyes_${n}.png`),
+  mouth: genArray(1, 10).map(n =>
+    `${process.env.PUBLIC_URL}/assets/KittyAvatar/mouth_${n}.png`),
+  fur: genArray(1, 10).map(n =>
+    `${process.env.PUBLIC_URL}/assets/KittyAvatar/fur_${n}.png`)
 };
 
 const dnaToAttributes = dna => {
