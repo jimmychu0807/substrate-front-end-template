4c7b909a101c7834517f8e673d35bf9c641c508d
Jimmy Chu <jimmy@parity.io>
2021-10-02 19:38:49 +0800

    Updated events with latest polkadot-js api (#200)
    * Updated events with latest polkadot-js api
    
    * Updated React version
    
    * Revert to use Yarn v1

--- src/Events.js
+++ src/Events.js
@@ -5,9 +5,20 @@ import { useSubstrate } from './substrate-lib';
 
 // Events to be filtered from feed
 const FILTERED_EVENTS = [
-  'system:ExtrinsicSuccess:: (phase={"ApplyExtrinsic":0})',
-  'system:ExtrinsicSuccess:: (phase={"ApplyExtrinsic":1})'
+  'system:extrinsicSuccess::(phase={"applyExtrinsic":0})'
 ];
+
+const eventName = ev => {
+  const level1key = Object.keys(ev)[0];
+  const level2key = Object.keys(ev[level1key])[0];
+  return `${level1key}:${level2key}`;
+};
+
+const eventParams = ev => {
+  const level1key = Object.keys(ev)[0];
+  const level2key = Object.keys(ev[level1key])[0];
+  return JSON.stringify(ev[level1key][level2key]);
+};
 
 function Main (props) {
   const { api } = useSubstrate();
@@ -15,32 +26,30 @@ function Main (props) {
 
   useEffect(() => {
     let unsub = null;
+    let keyNum = 0;
     const allEvents = async () => {
       unsub = await api.query.system.events(events => {
         // loop through the Vec<EventRecord>
         events.forEach(record => {
           // extract the phase, event and the event types
           const { event, phase } = record;
-          const types = event.typeDef;
 
           // show what we are busy with
-          const eventName = `${event.section}:${
-            event.method
-          }:: (phase=${phase.toString()})`;
+          const evjson = event.toJSON();
+          const evName = eventName(evjson);
+          const evParams = eventParams(evjson);
+          const evNamePhase = `${evName}::(phase=${phase.toString()})`;
 
-          if (FILTERED_EVENTS.includes(eventName)) return;
-
-          // loop through each of the parameters, displaying the type and data
-          const params = event.data.map(
-            (data, index) => `${types[index].type}: ${data.toString()}`
-          );
+          if (FILTERED_EVENTS.includes(evNamePhase)) return;
 
           setEventFeed(e => [{
+            key: keyNum,
             icon: 'bell',
-            summary: `${eventName}-${e.length}`,
-            extraText: event.meta.docs.join(', ').toString(),
-            content: params.join(', ')
+            summary: evName,
+            content: evParams
           }, ...e]);
+
+          keyNum += 1;
         });
       });
     };
