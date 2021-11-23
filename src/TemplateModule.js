79849c9273e8a71f21335510a2d2e598320ae260
Sacha Lansky <sacha@parity.io>
2021-11-23 17:53:29 +0100

    Fix lint console warnings

--- src/TemplateModule.js
+++ src/TemplateModule.js
@@ -1,70 +1,70 @@
 // React and Semantic UI elements.
-import React, { useState, useEffect } from 'react'
-import { Form, Input, Grid, Message } from 'semantic-ui-react'
+import React, { useState, useEffect } from 'react';
+import { Form, Input, Grid, Message } from 'semantic-ui-react';
 
 // Pre-built Substrate front-end utilities for connecting to a node
 // and making a transaction.
-import { useSubstrate } from './substrate-lib'
-import { TxButton } from './substrate-lib/components'
+import { useSubstrate } from './substrate-lib';
+import { TxButton } from './substrate-lib/components';
 
 // Polkadot-JS utilities for hashing data.
-import { blake2AsHex } from '@polkadot/util-crypto'
+import { blake2AsHex } from '@polkadot/util-crypto';
 
 // Main Proof Of Existence component is exported.
-export function Main(props) {
+export function Main (props) {
   // Establish an API to talk to the Substrate node.
-  const { api } = useSubstrate()
+  const { api } = useSubstrate();
   // Get the selected user from the `AccountSelector` component.
-  const { accountPair } = props
+  const { accountPair } = props;
   // React hooks for all the state variables we track.
   // Learn more at: https://reactjs.org/docs/hooks-intro.html
-  const [status, setStatus] = useState('')
-  const [digest, setDigest] = useState('')
-  const [owner, setOwner] = useState('')
-  const [block, setBlock] = useState(0)
+  const [status, setStatus] = useState('');
+  const [digest, setDigest] = useState('');
+  const [owner, setOwner] = useState('');
+  const [block, setBlock] = useState(0);
   // Our `FileReader()` which is accessible from our functions below.
-  let fileReader
+  let fileReader;
   // Takes our file, and creates a digest using the Blake2 256 hash function
   const bufferToDigest = () => {
     // Turns the file content to a hexadecimal representation.
     const content = Array.from(new Uint8Array(fileReader.result))
       .map(b => b.toString(16).padStart(2, '0'))
-      .join('')
-    const hash = blake2AsHex(content, 256)
-    setDigest(hash)
-  }
+      .join('');
+    const hash = blake2AsHex(content, 256);
+    setDigest(hash);
+  };
 
   // Callback function for when a new file is selected.
   const handleFileChosen = file => {
-    fileReader = new FileReader()
-    fileReader.onloadend = bufferToDigest
-    fileReader.readAsArrayBuffer(file)
-  }
+    fileReader = new FileReader();
+    fileReader.onloadend = bufferToDigest;
+    fileReader.readAsArrayBuffer(file);
+  };
 
   // React hook to update the owner and block number information for a file
   useEffect(() => {
-    let unsubscribe
+    let unsubscribe;
     // Polkadot-JS API query to the `proofs` storage item in our pallet.
     // This is a subscription, so it will always get the latest value,
     // even if it changes.
     api.query.templateModule
       .proofs(digest, result => {
         // Our storage item returns a tuple, which is represented as an array.
-        setOwner(result[0].toString())
-        setBlock(result[1].toNumber())
+        setOwner(result[0].toString());
+        setBlock(result[1].toNumber());
       })
       .then(unsub => {
-        unsubscribe = unsub
-      })
-    return () => unsubscribe && unsubscribe()
+        unsubscribe = unsub;
+      });
+    return () => unsubscribe && unsubscribe();
     // This tells the React hook to update whenever the file digest changes
     // (when a new file is chosen), or when the storage subscription says the
     // value of the storage item has updated.
-  }, [digest, api.query.templateModule])
+  }, [digest, api.query.templateModule]);
 
   // We can say a file digest is claimed if the stored block number is not 0
-  function isClaimed() {
-    return block !== 0
+  function isClaimed () {
+    return block !== 0;
   }
 
   // The actual UI elements which are returned from our component.
@@ -103,7 +103,7 @@ export function Main(props) {
               palletRpc: 'templateModule',
               callable: 'createClaim',
               inputParams: [digest],
-              paramFields: [true],
+              paramFields: [true]
             }}
           />
           {/* Button to revoke a claim. Only active if a file is selected, and is already claimed. Updates the `status`. */}
@@ -117,7 +117,7 @@ export function Main(props) {
               palletRpc: 'templateModule',
               callable: 'revokeClaim',
               inputParams: [digest],
-              paramFields: [true],
+              paramFields: [true]
             }}
           />
         </Form.Field>
@@ -125,12 +125,14 @@ export function Main(props) {
         <div style={{ overflowWrap: 'break-word' }}>{status}</div>
       </Form>
     </Grid.Column>
-  )
+  );
 }
 
-export default function TemplateModule(props) {
-  const { api } = useSubstrate()
-  return api.query.templateModule && api.query.templateModule.proofs ? (
+export default function TemplateModule (props) {
+  const { api } = useSubstrate();
+  return api.query.templateModule && api.query.templateModule.proofs
+    ? (
     <Main {...props} />
-  ) : null
+      )
+    : null;
 }
