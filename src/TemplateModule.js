fac56c25bb21b63020ab392e3d13ee4436e086c5
Sacha Lansky <sacha@parity.io>
2021-11-23 16:53:23 +0100

    Add PoE react component

--- src/TemplateModule.js
+++ src/TemplateModule.js
@@ -1,73 +1,70 @@
 // React and Semantic UI elements.
-import React, { useState, useEffect } from 'react';
-import { Form, Input, Grid, Message } from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react'
+import { Form, Input, Grid, Message } from 'semantic-ui-react'
+
 // Pre-built Substrate front-end utilities for connecting to a node
 // and making a transaction.
-import { useSubstrate } from './substrate-lib';
-import { TxButton } from './substrate-lib/components';
-// Polkadot-JS utilities for hashing data.
-import { blake2AsHex } from '@polkadot/util-crypto';
+import { useSubstrate } from './substrate-lib'
+import { TxButton } from './substrate-lib/components'
 
-// Our main Proof Of Existence Component which is exported.
-export function Main (props) {
-  // Establish an API to talk to our Substrate node.
-  const { api } = useSubstrate();
+// Polkadot-JS utilities for hashing data.
+import { blake2AsHex } from '@polkadot/util-crypto'
+
+// Main Proof Of Existence component is exported.
+export function Main(props) {
+  // Establish an API to talk to the Substrate node.
+  const { api } = useSubstrate()
   // Get the selected user from the `AccountSelector` component.
-  const { accountPair } = props;
+  const { accountPair } = props
   // React hooks for all the state variables we track.
   // Learn more at: https://reactjs.org/docs/hooks-intro.html
-  const [status, setStatus] = useState('');
-  const [digest, setDigest] = useState('');
-  const [owner, setOwner] = useState('');
-  const [block, setBlock] = useState(0);
-
+  const [status, setStatus] = useState('')
+  const [digest, setDigest] = useState('')
+  const [owner, setOwner] = useState('')
+  const [block, setBlock] = useState(0)
   // Our `FileReader()` which is accessible from our functions below.
-  let fileReader;
-
-  // Takes our file, and creates a digest using the Blake2 256 hash function.
+  let fileReader
+  // Takes our file, and creates a digest using the Blake2 256 hash function
   const bufferToDigest = () => {
     // Turns the file content to a hexadecimal representation.
     const content = Array.from(new Uint8Array(fileReader.result))
-      .map((b) => b.toString(16).padStart(2, '0'))
-      .join('');
-
-    const hash = blake2AsHex(content, 256);
-    setDigest(hash);
-  };
+      .map(b => b.toString(16).padStart(2, '0'))
+      .join('')
+    const hash = blake2AsHex(content, 256)
+    setDigest(hash)
+  }
 
   // Callback function for when a new file is selected.
-  const handleFileChosen = (file) => {
-    fileReader = new FileReader();
-    fileReader.onloadend = bufferToDigest;
-    fileReader.readAsArrayBuffer(file);
-  };
+  const handleFileChosen = file => {
+    fileReader = new FileReader()
+    fileReader.onloadend = bufferToDigest
+    fileReader.readAsArrayBuffer(file)
+  }
 
-  // React hook to update the owner and block number information for a file.
+  // React hook to update the owner and block number information for a file
   useEffect(() => {
-    let unsubscribe;
-
+    let unsubscribe
     // Polkadot-JS API query to the `proofs` storage item in our pallet.
     // This is a subscription, so it will always get the latest value,
     // even if it changes.
     api.query.templateModule
-      .proofs(digest, (result) => {
+      .proofs(digest, result => {
         // Our storage item returns a tuple, which is represented as an array.
-        setOwner(result[0].toString());
-        setBlock(result[1].toNumber());
+        setOwner(result[0].toString())
+        setBlock(result[1].toNumber())
       })
-      .then((unsub) => {
-        unsubscribe = unsub;
-      });
-
-    return () => unsubscribe && unsubscribe();
+      .then(unsub => {
+        unsubscribe = unsub
+      })
+    return () => unsubscribe && unsubscribe()
     // This tells the React hook to update whenever the file digest changes
     // (when a new file is chosen), or when the storage subscription says the
     // value of the storage item has updated.
-  }, [digest, api.query.templateModule]);
+  }, [digest, api.query.templateModule])
 
-  // We can say a file digest is claimed if the stored block number is not 0.
-  function isClaimed () {
-    return block !== 0;
+  // We can say a file digest is claimed if the stored block number is not 0
+  function isClaimed() {
+    return block !== 0
   }
 
   // The actual UI elements which are returned from our component.
@@ -79,50 +76,48 @@ export function Main (props) {
         <Form.Field>
           {/* File selector with a callback to `handleFileChosen`. */}
           <Input
-            type='file'
-            id='file'
-            label='Your File'
-            onChange={ e => handleFileChosen(e.target.files[0]) }
+            type="file"
+            id="file"
+            label="Your File"
+            onChange={e => handleFileChosen(e.target.files[0])}
           />
           {/* Show this message if the file is available to be claimed */}
-          <Message success header='File Digest Unclaimed' content={digest} />
+          <Message success header="File Digest Unclaimed" content={digest} />
           {/* Show this message if the file is already claimed. */}
           <Message
             warning
-            header='File Digest Claimed'
+            header="File Digest Claimed"
             list={[digest, `Owner: ${owner}`, `Block: ${block}`]}
           />
         </Form.Field>
         {/* Buttons for interacting with the component. */}
         <Form.Field>
-          {/* Button to create a claim. Only active if a file is selected,
-          and not already claimed. Updates the `status`. */}
+          {/* Button to create a claim. Only active if a file is selected, and not already claimed. Updates the `status`. */}
           <TxButton
             accountPair={accountPair}
             label={'Create Claim'}
             setStatus={setStatus}
-            type='SIGNED-TX'
+            type="SIGNED-TX"
             disabled={isClaimed() || !digest}
             attrs={{
               palletRpc: 'templateModule',
               callable: 'createClaim',
               inputParams: [digest],
-              paramFields: [true]
+              paramFields: [true],
             }}
           />
-          {/* Button to revoke a claim. Only active if a file is selected,
-          and is already claimed. Updates the `status`. */}
+          {/* Button to revoke a claim. Only active if a file is selected, and is already claimed. Updates the `status`. */}
           <TxButton
             accountPair={accountPair}
-            label='Revoke Claim'
+            label="Revoke Claim"
             setStatus={setStatus}
-            type='SIGNED-TX'
+            type="SIGNED-TX"
             disabled={!isClaimed() || owner !== accountPair.address}
             attrs={{
               palletRpc: 'templateModule',
               callable: 'revokeClaim',
               inputParams: [digest],
-              paramFields: [true]
+              paramFields: [true],
             }}
           />
         </Form.Field>
@@ -130,12 +125,12 @@ export function Main (props) {
         <div style={{ overflowWrap: 'break-word' }}>{status}</div>
       </Form>
     </Grid.Column>
-  );
+  )
 }
 
-export default function TemplateModule (props) {
-  const { api } = useSubstrate();
-  return api.query.templateModule && api.query.templateModule.proofs
-    ? <Main {...props} />
-    : null;
+export default function TemplateModule(props) {
+  const { api } = useSubstrate()
+  return api.query.templateModule && api.query.templateModule.proofs ? (
+    <Main {...props} />
+  ) : null
 }
