7909cb5171a2efa2f7fb5478caeaa85abb4914ff
= <andrew.tarr@hotmail.com>
2022-01-20 17:43:25 -0600

    More merging main

--- src/substrate-lib/components/developerConsole.ts
+++ src/substrate-lib/components/developerConsole.ts
@@ -1,12 +1,17 @@
 // This component will simply add utility functions to your developer console.
-import { useSubstrate } from '../Index';
+import { useSubstrate } from '../';
 
 export default function DeveloperConsole (props) {
   const { api, apiState, keyring, keyringState } = useSubstrate();
   const windowRef = window as any;
 
-  if (apiState === 'READY') { windowRef.api = api; }
-  if (keyringState === 'READY') { windowRef.keyring = keyring; }
+  if (apiState === 'READY')  { 
+    windowRef.api = api; 
+  }
+  
+  if (keyringState === 'READY') { 
+    windowRef.keyring = keyring; 
+  }
   windowRef.util = require('@polkadot/util');
   windowRef.utilCrypto = require('@polkadot/util-crypto');
 
