50f2a18239cfbfd06c438606125e10bf353c68fb
Nikos Kontakis <wirednkod@gmail.com>
2023-10-08 16:54:05 +0300

    Add light client support for test networks of Westend and Rococo

--- src/NodeInfo.js
+++ src/NodeInfo.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { Card, Icon, Grid } from 'semantic-ui-react'
+import { Form, Card, Icon, Grid } from 'semantic-ui-react'
 
 import { useSubstrateState } from './substrate-lib'
+const INIT_URL = '/substrate-front-end-template'
 
 function Main(props) {
   const { api, socket } = useSubstrateState()
   const [nodeInfo, setNodeInfo] = useState({})
+  const [conn] = useState(window.location.href.split('?')[1] || undefined)
 
   useEffect(() => {
     const getInfo = async () => {
@@ -27,11 +29,43 @@ function Main(props) {
     <Grid.Column>
       <Card>
         <Card.Content>
+          <Form>
+            <Form.Group style={{ overflowX: 'auto', flexDirection: 'column' }}>
+              <label>Connect:</label>
+              <Form.Radio
+                label="Local"
+                name="localNode"
+                value="localNode"
+                checked={!conn}
+                onChange={() => {
+                  window.location.href = INIT_URL
+                }}
+              />
+              <Form.Radio
+                label="Westend Light Client"
+                name="westend_lc"
+                value="westend_lc"
+                checked={conn === 'westend_light_client'}
+                onChange={() => {
+                  window.location.href = INIT_URL + '?westend_light_client'
+                }}
+              />
+              <Form.Radio
+                label="Rococo Light Client"
+                name="rococo_lc"
+                value="rococo_lc"
+                checked={conn === 'rococo_light_client'}
+                onChange={() => {
+                  window.location.href = INIT_URL + '?rococo_light_client'
+                }}
+              />
+            </Form.Group>
+          </Form>
           <Card.Header>{nodeInfo.nodeName}</Card.Header>
           <Card.Meta>
             <span>{nodeInfo.chain}</span>
           </Card.Meta>
-          <Card.Description>{socket}</Card.Description>
+          {!conn ? <Card.Description>{socket}</Card.Description> : null}
         </Card.Content>
         <Card.Content extra>
           <Icon name="setting" />v{nodeInfo.nodeVersion}
