ac335275e8a0567f7a64ad29aee10cb0ec456c51
fzeeshan <faisalzeeshan2012@gmail.com>
2024-04-11 22:44:58 -0500

    Update Transfer.js

--- src/Transfer.js
+++ src/Transfer.js
@@ -81,7 +81,7 @@ export default function Main(props) {
             setStatus={setStatus}
             attrs={{
               palletRpc: 'balances',
-              callable: 'transfer',
+              callable: 'transferAllowDeath', //changed from transfer to transferAllowDeath
               inputParams: [addressTo, amount],
               paramFields: [true, true],
             }}
